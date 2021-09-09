9cd9fe6707425475d2a946062ba0b4829b829617
laoliu <liushuo@lewei50.com>
2021-09-09 10:52:24 +0800

    add demo 2

---
+++ demo2/js/config.js
@@ -0,0 +1,7 @@
+var sns =[
+    {"sn":"DA2BED94","name":"Iammeter","type":"1","items":[2]},
+    {"sn":"674B1E5D","name":"674B1E5D","type":"3","items":[[1,2]]}
+];
+
+var username = 'lewei';
+var password = '123456';
