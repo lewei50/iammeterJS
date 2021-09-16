4905ce74c9844fafdc2d9a45dc3483c8f7128bd1
laoliu <liushuo@lewei50.com>
2021-09-16 15:41:05 +0800

    config.js

--- Demo1/js/config.js
+++ Demo1/js/config.js
@@ -1,6 +1,5 @@
 var sns =[
-    {"sn":"DA2BED94","name":"Iammeter","type":"1","items":[2]},
-    {"sn":"674B1E5D","name":"674B1E5D","type":"3","items":[[1,2],[2,2]]}
+    {"sn":"DA2BED94","name":"Iammeter","type":"1","items":[2]}
 ];
 
 var username = 'lewei';
